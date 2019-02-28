46b09cd3d1013f3801f1dd25c217e682077a0a3a
maksim_kolesnik <maksim.kolesnik@emka3.de>
2019-02-28 22:15:45 +0100

    working code with config init.

--- src/Mobile/Emka.PracticeLooper.Mobile/App.xaml.cs
+++ src/Mobile/Emka.PracticeLooper.Mobile/App.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using Emka3.PracticeLooper.Mappings;
+using Emka3.PracticeLooper.Services.Contracts.Common;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,10 +11,13 @@ namespace Emka.PracticeLooper.Mobile
 {
     public partial class App : Application
     {
+        public static IConfigurationService ConfigurationService { get; private set; }
+
         public App()
         {
             InitializeComponent();
-
+            var config = Factory.GetResolver().Resolve<IConfigurationService>();
+            InitApp();
             MainPage = new MainPage();
         }
 
@@ -27,6 +34,26 @@ namespace Emka.PracticeLooper.Mobile
         protected override void OnResume()
         {
             // Handle when your app resumes
+        }
+
+        private void InitApp()
+        {
+            try
+            {
+                string jsonConfig;
+                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Emka.PracticeLooper.Mobile.App.config.json"))
+                using (var reader = new StreamReader(stream))
+                {
+                    jsonConfig = reader.ReadToEnd();
+                }
+
+                ConfigurationService = Factory.GetResolver().Resolve<IConfigurationService>();
+                ConfigurationService.Initialize(jsonConfig);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
