232093fa09241292573c8d44fff252bfd424cd37
maksim_kolesnik <maksim.kolesnik@emka3.de>
2019-03-24 18:42:35 +0100

    android version plays audio files

--- src/Mobile/Emka.PracticeLooper.Mobile.Android/Common/FileAudioPlayer.cs
+++ src/Mobile/Emka.PracticeLooper.Mobile.Android/Common/FileAudioPlayer.cs
@@ -3,6 +3,7 @@
 // Proprietary and confidential
 // Maksim Kolesnik maksim.kolesnik@emka3.de, 2019
 using System;
+using Android.Media;
 using Emka3.PracticeLooper.Model.Player;
 using Emka3.PracticeLooper.Services.Contracts.Player;
 
@@ -10,18 +11,24 @@ namespace Emka.PracticeLooper.Mobile.Droid.Common
 {
     public class FileAudioPlayer : IAudioPlayer
     {
+        #region Fields
+        MediaPlayer player;
+        Session session;
+        #endregion
+
         #region Ctor
         public FileAudioPlayer()
         {
+            player = new MediaPlayer();
         }
         #endregion
 
         #region Properties
-        public bool IsPlaying => throw new NotImplementedException();
+        public bool IsPlaying => player.IsPlaying;
 
-        public double SongDuration => throw new NotImplementedException();
+        public double SongDuration => 0.0;
 
-        public Loop CurrentLoop { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Loop CurrentLoop { get; set; }
 
         public event EventHandler<bool> PlayStatusChanged;
         #endregion
@@ -29,22 +36,67 @@ namespace Emka.PracticeLooper.Mobile.Droid.Common
         #region Methods
         public void Init(Session session)
         {
-            throw new NotImplementedException();
+            this.session = session;
+            CurrentLoop = session.Loops[0];
+            CurrentLoop.StartPositionChanged += OnStartPositionChanged;
+            CurrentLoop.EndPositionChanged += OnEndPositionChanged;
+            player.Reset();
+            player.SetDataSource(session.AudioSource.Source);
+            player.Prepare();
+            //var asset = AVAsset.FromUrl(new NSUrl(new Uri(session.AudioSource.Source).AbsoluteUri));
+            //var playerItem = new AVPlayerItem(asset);
+            //audioPlayer = new AVPlayer(playerItem);
+            //var playerLayer = AVPlayerLayer.FromPlayer(audioPlayer);
+
+            //TimeObserver = audioPlayer.AddBoundaryTimeObserver(new[] { NSValue.FromCMTime(CMTime.FromSeconds(CurrentLoop.EndPosition * SongDuration, 1)) }, null, () =>
+            //{
+            //    Seek(CurrentLoop.StartPosition);
+            //});
+            //player.Play();
+
+            //audioPlayer = AVAudioPlayer.FromUrl(new NSUrl(new Uri(audioSource.Source).AbsoluteUri));
+            ////audioPlayer2.Set
+            //audioPlayer.PrepareToPlay();
+            //SongDuration = audioPlayer.CurrentItem.Asset.Duration.Seconds;
+        }
+
+        private void OnEndPositionChanged(object sender, double e)
+        {
+
+        }
+
+        private void OnStartPositionChanged(object sender, double e)
+        {
+
         }
 
         public void Pause()
         {
-            throw new NotImplementedException();
+            if (IsPlaying)
+            {
+                player.Pause();
+                RaisePlayingStatusChanged();
+            }
         }
 
         public void Play()
         {
-            throw new NotImplementedException();
+            if (!IsPlaying)
+            {
+                player.Start();
+                RaisePlayingStatusChanged();
+            }
         }
 
         public void Seek(double time)
         {
-            throw new NotImplementedException();
+            var position = TimeSpan.FromMilliseconds(time);
+            player.SeekTo(position.Milliseconds);
+        }
+
+        private void RaisePlayingStatusChanged()
+        {
+            PlayStatusChanged?.Invoke(this, IsPlaying);
         }
         #endregion
     }
