5098b8ea20a7d3605d0bb3011b4b9f5be11c299e
simonsymhoven <post@simon-symhoven.de>
2020-07-10 14:08:04 +0200

    add post clone scripts for iOS and Android

---
+++ Mobile/Emka.PracticeLooper.Mobile.iOS/appcenter-post-clone.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "******************************************************************************"
+echo "*************** Custom Post Clone Script -- iOS Edition **********************"
+echo "******************************************************************************"
+
+echo "************************* Build secrets.json iOS *****************************"
+
+secrets_location=$APPCENTER_SOURCE_DIRECTORY/Mobile/Emka3.PracticeLooper.Config/secrets.json
+echo "Write file to $secrets_location"
+
+echo "{SpotifyClientSecret:\"$Secret_SpotifyClientSecret\", SpotifyClientAuthUri:\"$Secret_SpotifyClientAuthUri\", SpotifyClientTokenUri:\"$Secret_SpotifyClientTokenUri\", SpotifyClientUseNativeUi: $Secret_SpotifyClientUseNativeUi, AdmobIosAppId:\"$Secret_AdmobIosAppId\", AdmobIosTopBannerAdId:\"$Secret_AdmobIosTopBannerAdId\", AdmobIosInterstitialProjectAdId:\"$Secret_AdmobIosInterstitialProjectAdId\", AppCenterIos:\"$Secret_AppCenterIos\", , SpotifyApiLimit: $Secret_SpotifyApiLimit, SpotifyClientId:\"$Secret_SpotifyClientId\", SpotifyClientScopes:\"$Secret_SpotifyClientScopes\", SpotifyClientApiUri:\"$Secret_SpotifyClientApiUri\", SpotifyClientRedirectUri:\"$Secret_SpotifyClientRedirectUri\", SpotifyClientRequestCode: $Secret_SpotifyClientRequestCode, AdmobAndroidAppId:\"$Secret_AdmobAndroidAppId\", AppCenterAndroid:\"$Secret_AppCenterAndroid\", PublicKey1:\"$Secret_PublicKey1\", PublicKey2:\"$Secret_PublicKey2\", PublicKey3:\"$Secret_PublicKey3\", AdmobAndroidTopBanneAdId:\"$Secret_AdmobAndroidTopBanneAdId\", AdmobAndroidInterstitialProjectAdId:\"$Secret_AdmobAndroidInterstitialProjectAdId\", DbName:\"$Secret_DbName\", InAppIosPremiumGeneral:\"$Secret_InAppIosPremiumGeneral\", InAppAndroidPremiumGeneral:\"$Secret_InAppAndroidPremiumGeneral\", SyncFusionLicenseKey:\"$Secret_SyncFusionLicenseKey\"}" > $secrets_location
+
+echo "**************************** secrets.json iOS *******************************"
+cat $secrets_location
