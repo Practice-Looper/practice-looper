cdff5f09b877692e03c1dd14546a355a249c01eb
simonsymhoven <post@simon-symhoven.de>
2020-07-10 14:07:04 +0200

    add Secrets.cs wrapper to vcs delete Mobile.BuildTools

---
+++ Mobile/Emka3.PracticeLooper.Config/Secrets.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (C) Emka3 - All Rights Reserved
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+// simonsymhoven post@simon-symhoven.de, 2020
+using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Emka3.PracticeLooper.Config
+{
+    public static class Secrets
+    {
+        public static dynamic GetSecrets()
+        {
+            string json = File.ReadAllText("secrets.json");
+            return JsonConvert.DeserializeObject(json);
+
+        }
+    }
+}
