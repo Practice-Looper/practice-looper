e370358153e14bad1a9cc435b424c4ba5f14d6e3
simonsymhoven <post@simon-symhoven.de>
2020-07-23 11:55:37 +0200

    add APPCENTER_ACCESS_TOKEN to each appcenter cli call

--- Mobile/Emka.PracticeLooper.Mobile.Android/appcenter-post-build.sh
+++ Mobile/Emka.PracticeLooper.Mobile.Android/appcenter-post-build.sh
@@ -8,5 +8,6 @@ echo "********************** Distribute Release Android ************************
 
 aab_file=$APPCENTER_SOURCE_DIRECTORY/Mobile/Emka3.PracticeLooper.Mobile.Android/bin/Release/de.emka3.practice_looper-Signed.aab
 
-
-appcenter distribute release --app emka3/PracticeLooper --file $aab_file --group "Collaborators"
+appcenter apps list --token $appcenter_token_simon
+echo appcenter apps list --token $appcenter_token_simon
+appcenter distribute release --app emka3/PracticeLooper --file $aab_file --group "Collaborators" --token $appcenter_token_simon
