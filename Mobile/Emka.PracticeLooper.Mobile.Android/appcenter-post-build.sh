2de46c4a9f20185ffdf6f2bda4237150bc2be740
simonsymhoven <post@simon-symhoven.de>
2020-07-23 11:42:58 +0200

    modify post build script

--- Mobile/Emka.PracticeLooper.Mobile.Android/appcenter-post-build.sh
+++ Mobile/Emka.PracticeLooper.Mobile.Android/appcenter-post-build.sh
@@ -4,9 +4,9 @@ echo "**************************************************************************
 echo "************* Custom Post Build Script -- Android Edition ********************"
 echo "******************************************************************************"
 
-echo "********************** Build secrets.json Android ****************************"
+echo "********************** Distribute Release Android ****************************"
 
 aab_file=$APPCENTER_SOURCE_DIRECTORY/Mobile/Emka3.PracticeLooper.Mobile.Android/bin/Release/de.emka3.practice_looper-Signed.aab
 
 
-appcenter distribute release --app emka3/Practice Looper --file $aab_file --group "Collaborators"
+appcenter distribute release --app emka3/PracticeLooper --file $aab_file --group "Collaborators"
