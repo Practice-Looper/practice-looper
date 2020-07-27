4aea6872ebc0f01b0fa0de2940d3d17128dbfffe
simonsymhoven <post@simon-symhoven.de>
2020-07-27 22:37:06 +0200

    bugfixing for reading testresult of xunit trx file

--- Mobile/Emka.PracticeLooper.Mobile.iOS/appcenter-pre-build.sh
+++ Mobile/Emka.PracticeLooper.Mobile.iOS/appcenter-pre-build.sh
@@ -6,14 +6,12 @@ echo "**************************************************************************
 
 echo "Running a search for XUnit test projects:"
 
-array=(`find $APPCENTER_SOURCE_DIRECTORY -regex '.*Tests*.csproj'`)
+array=(`find $APPCENTER_SOURCE_DIRECTORY\Mobile -regex '.*Tests*.csproj'`)
 
 for projectFile in "${array[@]}"
 do :
-	echo "**************************************"
 	echo "Project:  ${projectFile}" 	
     	echo "Assembly: $(dirname "${projectFile}")"
-	echo "**************************************"
 done
 
 echo "Running XUnit tests:"
@@ -23,21 +21,19 @@ do :
 	cd $(dirname "${projectFile}")
 	dotnet test --logger "trx;LogFileName=xunitestresults.trx";
 	
-	
 	echo "Analyse Log-File:"
 	testResultPath=$(find $(dirname "${projectFile}") -name 'xunitestresults.trx')
-	echo $testResultPath
-	if [[ $testResultPath -eq "" ]]
+	
+	if [ -z "$testResultPath" ]
 	then
 		echo "Found no testresults"
 		echo "Unit Tests for project $(basename "${projectFile}") failed!"
 		exit 1
 	else 
 		echo "Found testresult"
-		grep ' [FAIL]' $testResultPath
-		failures=$(grep -o ' [FAIL]' $testResultPath | wc -l)
-
-		if [[ $failures -eq 0 ]]
+		failures=$(grep -o 'failed="0"' $testResultPath | wc -l)
+		
+		if [[ $failures -eq 1 ]]
 		then
 			echo "Unit Tests for project $(basename "${projectFile}") passed!"
 		else
