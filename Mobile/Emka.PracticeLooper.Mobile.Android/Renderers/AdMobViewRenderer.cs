1f2cd666729b03b4f4f59cb29c30aab5b7407ef0
simonsymhoven <post@simon-symhoven.de>
2020-07-30 21:44:07 +0200

    new adid to android admob remove AppCenterReleasePremium Config

--- Mobile/Emka.PracticeLooper.Mobile.Android/Renderers/AdMobViewRenderer.cs
+++ Mobile/Emka.PracticeLooper.Mobile.Android/Renderers/AdMobViewRenderer.cs
@@ -52,7 +52,7 @@ namespace Emka.PracticeLooper.Mobile.Droid.Renderers
                 AdRequest request;
                 request = new AdRequest.Builder()
                     .AddTestDevice("F05EA53323F18138CB1722DD1F00A0F0")
-                    .AddTestDevice("05EAB5D891232107FF441DD3903FD547")
+                    .AddTestDevice("B0B92426A54BF08C56BC62D5703161F3")
                     .Build();
                 adView.LoadAd(request);
             }
