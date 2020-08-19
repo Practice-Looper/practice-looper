334a67e1639297c11c1affc72513905cab0cbd3c
simonsymhoven <post@simon-symhoven.de>
2020-08-19 22:56:55 +0200

    change branch to master

--- Mobile/Emka.PracticeLooper.Mobile.iOS/appcenter-post-build.sh
+++ Mobile/Emka.PracticeLooper.Mobile.iOS/appcenter-post-build.sh
@@ -5,7 +5,7 @@ echo "**************** Custom Post Build Script -- iOS Edition *****************
 echo "******************************************************************************"
 
 
-if [ "$APPCENTER_BRANCH" == "issue#51-git-version-bump" ];
+if [ "$APPCENTER_BRANCH" == "master" ];
 then
   REPO_URL=https://${GIT_ACCESS_TOKEN_SIMON}@github.com/emka3/practice-looper.git
   INFO_PLIST_PATH=$APPCENTER_SOURCE_DIRECTORY/Mobile/Emka.PracticeLooper.Mobile.iOS/Info.plist
@@ -16,7 +16,7 @@ then
 
   VERSION_STRING=$(grep -o -A1 '<key>CFBundleVersion</key>' $INFO_PLIST_PATH | grep -o '[0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,2\}')
   git commit -m "[AppCenter] Bump version to ${VERSION_STRING}"
-  git push ${REPO_URL} HEAD:issue\#51-git-version-bump
+  git push ${REPO_URL} HEAD:master
 
   echo "-------"
   echo "Push tag to origin"
