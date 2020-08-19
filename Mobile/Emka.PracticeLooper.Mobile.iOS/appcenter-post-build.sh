6593012778c15197b84a71294f419c8acda0fb19
simonsymhoven <post@simon-symhoven.de>
2020-08-19 21:57:47 +0200

    reowrk of post build script

--- Mobile/Emka.PracticeLooper.Mobile.iOS/appcenter-post-build.sh
+++ Mobile/Emka.PracticeLooper.Mobile.iOS/appcenter-post-build.sh
@@ -16,5 +16,8 @@ then
 
   VERSION_STRING=$(grep -o -A1 '<key>CFBundleVersion</key>' $INFO_PLIST_PATH | grep -o '[0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,2\}')
   git commit -m "[AppCenter] Bump version to ${VERSION_STRING}"
-  git push origin issue\#51-git-version-bump
+  git push ${REPO_URL} issue\#51-git-version-bump
+  git push ${REPO_URL}
+  git push origin
+  git tag -a v${VERSION_STRING} -m "Release ${VERSION_STRING}"
 fi
