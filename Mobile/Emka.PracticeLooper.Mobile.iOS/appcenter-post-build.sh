b9a9394435aef89aa0c0ac3b1fd4fa71f610b8de
simonsymhoven <post@simon-symhoven.de>
2020-08-19 21:26:38 +0200

    add appcenter-post-build script

---
+++ Mobile/Emka.PracticeLooper.Mobile.iOS/appcenter-post-build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo "******************************************************************************"
+echo "**************** Custom Post Build Script -- iOS Edition *********************"
+echo "******************************************************************************"
+
+
+if [ "$APPCENTER_BRANCH" == "issue#51-git-version-bump" ];
+then
+  REPO_URL=https://${GIT_ACCESS_TOKEN_SIMON}@github.com/emka3/practice-looper.git
+  INFO_PLIST_PATH=$APPCENTER_SOURCE_DIRECTORY/Mobile/Emka.PracticeLooper.Mobile.iOS/Info.plist
+  MANIFEST_PATH=$APPCENTER_SOURCE_DIRECTORY/Mobile/Emka.PracticeLooper.Mobile.Android/Properties/AndroidManifest.xml
+
+  git add ${INFO_PLIST_PATH}
+  git add ${MANIFEST_PATH}
+
+  VERSION_STRING=$(grep -o -A1 '<key>CFBundleVersion</key>' $INFO_PLIST_PATH | grep -o '[0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,2\}')
+  git commit -m "[AppCenter] Bump version to ${VERSION_STRING}"
+  git push origin issue\#51-git-version-bump
+fi
