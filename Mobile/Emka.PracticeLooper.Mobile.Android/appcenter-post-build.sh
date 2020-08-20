7d157c558ec79cbb2e98653aec36e96e6e1cc3b9
simonsymhoven <post@simon-symhoven.de>
2020-08-20 15:03:36 +0200

    Modify iOS script, add scipt for android

--- Mobile/Emka.PracticeLooper.Mobile.Android/appcenter-post-build.sh
+++ Mobile/Emka.PracticeLooper.Mobile.Android/appcenter-post-build.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 echo "******************************************************************************"
-echo "**************** Custom Post Build Script -- iOS Edition *********************"
+echo "************** Custom Post Build Script -- Android Edition *******************"
 echo "******************************************************************************"
 
 
 if [ "$APPCENTER_BRANCH" == "master" ];
 then
   REPO_URL=https://${GIT_ACCESS_TOKEN_SIMON}@github.com/emka3/practice-looper.git
-  INFO_PLIST_PATH=$APPCENTER_SOURCE_DIRECTORY/Mobile/Emka.PracticeLooper.Mobile.iOS/Info.plist
+  MANIFEST_PATH=$APPCENTER_SOURCE_DIRECTORY/Mobile/Emka.PracticeLooper.Mobile.Android/Properties/AndroidManifest.xml
 
-  git add ${INFO_PLIST_PATH}
+  git add ${MANIFEST_PATH}
 
-  VERSION_STRING=$(grep -o -A1 '<key>CFBundleVersion</key>' $INFO_PLIST_PATH | grep -o '[0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,2\}')
+  VERSION_STRING=$(grep -o 'android:versionName="*.*.*" ' $MANIFEST_PATH | grep -o '[0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,2\}')
   git commit -m "[AppCenter] Bump version to ${VERSION_STRING}"
   git push ${REPO_URL} HEAD:master
 
