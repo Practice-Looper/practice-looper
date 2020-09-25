c26da588d3707104817c2cd865bd9fb9eb72e392
maksim <maksim.kolesnik@emka3.de>
2020-09-25 14:05:36 +0200

    adds mocks

---
+++ libs/iOS/RevenueCat 3.5.3/RevenueCatProxy/build/RevenueCatProxy.build/Release-iphoneos/RevenueCatProxy.build/DerivedSources/RevenueCatProxy_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char RevenueCatProxyVersionString[];
+ extern const double RevenueCatProxyVersionNumber;
+
+ const unsigned char RevenueCatProxyVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:RevenueCatProxy  PROJECT:RevenueCatProxy-1" "\n";
+ const double RevenueCatProxyVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ libs/iOS/RevenueCat 3.5.3/RevenueCatProxy/build/RevenueCatProxy.build/Release-iphonesimulator/RevenueCatProxy.build/Objects-normal/x86_64/RevenueCatProxy_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Volumes/Work/Projekte/Mobile/practice-looper/libs/iOS/RevenueCat\ 3.5.3/RevenueCatProxy/build/RevenueCatProxy.build/Release-iphonesimulator/RevenueCatProxy.build/DerivedSources/RevenueCatProxy_vers.c
