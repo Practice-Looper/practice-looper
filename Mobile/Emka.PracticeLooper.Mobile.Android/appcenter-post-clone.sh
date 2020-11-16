34e519d704217c1dce180cc6d6182519066eb895
Simon Symhoven <post@simon-symhoven.de>
2020-11-16 17:44:53 +0100

    Organisation scripts (#104)
    * Change post clone scripts, update Github ccess token
    
    * Remove dotnet installation fomr android post clone script

--- Mobile/Emka.PracticeLooper.Mobile.Android/appcenter-post-clone.sh
+++ Mobile/Emka.PracticeLooper.Mobile.Android/appcenter-post-clone.sh
@@ -46,7 +46,3 @@ echo "{
 echo "************************** secrets.json Android *****************************"
 cat $secrets_location
 
-echo "************************ Install custom .NET Core ***************************"
-wget https://dot.net/v1/dotnet-install.sh
-chmod +x dotnet-install.sh
-./dotnet-install.sh --version $netcoreversion --install-dir "$AGENT_TOOLSDIRECTORY/dotnet"
