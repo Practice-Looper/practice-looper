0b812363f7fd5f184106831be1aa5d3a4224bcd4
simonsymhoven <post@simon-symhoven.de>
2020-11-16 13:04:54 +0100

    Change post clone scripts, update Github ccess token

--- Mobile/Emka.PracticeLooper.Mobile.Android/appcenter-post-build.sh
+++ Mobile/Emka.PracticeLooper.Mobile.Android/appcenter-post-build.sh
@@ -4,7 +4,6 @@ echo "**************************************************************************
 echo "************** Custom Post Build Script -- Android Edition *******************"
 echo "******************************************************************************"
 
-REPO_URL=https://${GIT_ACCESS_TOKEN_SIMON}@github.com/emka3/practice-looper.git
 MANIFEST_PATH=$APPCENTER_SOURCE_DIRECTORY/Mobile/Emka.PracticeLooper.Mobile.Android/Properties/AndroidManifest.xml
 
 if [ "$APPCENTER_BRANCH" == "master" ];
@@ -18,17 +17,17 @@ then
     git add ${APPCENTER_SOURCE_DIRECTORY}/coverage-reports/${APP_VERSION}
 
     git commit -m "bump version to ${VERSION_STRING}, add coverage-report for ${APP_VERSION}"
-    git push ${REPO_URL} HEAD:master
+    git push ${GIT_REPO_ACCESS} HEAD:master
 
     echo "Push tag to origin"
     git tag -a v${VERSION_STRING}-Android-${APPCENTER_XAMARIN_CONFIGURATION} -m "Android ${APPCENTER_XAMARIN_CONFIGURATION} ${VERSION_STRING}"
-    git push ${REPO_URL} v${VERSION_STRING}-Android-${APPCENTER_XAMARIN_CONFIGURATION}
+    git push ${GIT_REPO_ACCESS} v${VERSION_STRING}-Android-${APPCENTER_XAMARIN_CONFIGURATION}
   fi
 else
   if [ "$AGENT_JOBSTATUS" == "Succeeded" ];
   then
     git add ${APPCENTER_SOURCE_DIRECTORY}/coverage-reports/${APP_VERSION}
     git commit -m "add coverage-report for ${APP_VERSION}"
-    git push ${REPO_URL} HEAD:${APPCENTER_BRANCH}
+    git push ${GIT_REPO_ACCESS} HEAD:${APPCENTER_BRANCH}
   fi
 fi
